fb87a4672317cdcc6baadb31f3f66ae9ce9806d3
Shubham-Rasal <bluequbits@gmail.com>
2023-10-11 21:49:20 +0530

    vendor upload draft

--- client/src/app/vendor/page.js
+++ client/src/app/vendor/page.js
@@ -1,11 +1,11 @@
 import Link from "next/link"
 export default function VendorPage() {
     return (
-        <main className="flex p-24 flex-col">
+        <main className="flex p-24 flex-col bg-white">
             <h1 className="text-3xl">Vendor Page</h1>
             <div className="flex flex-row justify-between">
 
-                <Link href="/create">
+                <Link href="/vendor/create">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         Create New Job
                     </button>
