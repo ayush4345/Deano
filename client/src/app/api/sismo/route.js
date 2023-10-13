de85bb2e53882de661edc8fc682b4b8bea29f1b6
Shubham-Rasal <bluequbits@gmail.com>
2023-10-13 15:33:17 +0530

    added sismo test url

--- client/src/app/api/sismo/route.js
+++ client/src/app/api/sismo/route.js
@@ -1,9 +1,27 @@
 import { NextResponse } from 'next/server'
-
+import data from './data.json'
 export async function GET(request) {
 
-    const followers = await fetch(" https://api.github.com/users/Shubham-Rasal/followers")
-    const followersJson = await followers.json()
-    console.log(followersJson)
-    return NextResponse.json(followersJson)
+    let followers = data;
+    console.log(followers);
+
+    //construct a json response with this format
+    // {
+    //     "name" : "id"
+    // }
+
+    followers = data.map((follower) => {
+        return {
+            name: follower.login,
+            id: follower.id
+        }
+    })
+
+    const result = {}
+
+    followers.forEach((follower) => {
+        result[follower.name] = follower.id
+    })
+
+    return NextResponse.json(result)
 }
