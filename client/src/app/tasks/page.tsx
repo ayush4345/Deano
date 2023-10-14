79d12e48abfc1a54bd07cd9902261a902442b33d
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-10-14 12:37:01 +0530

    made ui for showing tasks

---
+++ client/src/app/tasks/page.tsx
@@ -0,0 +1,56 @@
+import { Metadata } from "next";
+import Image from "next/image";
+
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import { UserNav } from "./components/user-nav";
+
+// export const metadata: Metadata = {
+//   title: "Tasks",
+//   description: "A task and issue tracker build using Tanstack Table.",
+// }
+
+// Simulate a database read for tasks.
+
+export default async function TaskPage() {
+  const response = await fetch("http://localhost:3000/api/data");
+
+  const tasks = await response.json()
+
+  // console.log(tasks)
+
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of tasks!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
+}
