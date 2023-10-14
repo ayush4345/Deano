d99daa477071ce61171c4f0820c3de7567f5d766
Shubham-Rasal <bluequbits@gmail.com>
2023-10-14 21:33:59 +0530

    removed backend

--- backend/contracts/Faucet.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;'
-
-
-interface Faucet {
-        requestTokens(uint256 amount) external;
-        tranferToPool(uint256 amount) external;
-}
