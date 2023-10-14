e4c9d00f824356f7b70af20bee1e969ec0b2c7d5
Shubham-Rasal <bluequbits@gmail.com>
2023-10-14 21:35:00 +0530

    added sismo token payout

--- client/src/app/faucet/page.js
+++ client/src/app/faucet/page.js
@@ -1,12 +1,11 @@
 'use client'
-// import { db, healthCheck, createTable, insertData, readData } from '../../tableland/connect.js'
-
+import { db, healthCheck, createTable, insertData, readData } from '../../tableland/connect.js'
 
 export default function Faucet() {
 
     return (
         <h2 className='text-center'>
-            {/* <button onClick={healthCheck} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            <button onClick={healthCheck} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
                 Connect to Tableland
             </button>
 
@@ -20,7 +19,7 @@ export default function Faucet() {
 
             <button onClick={readData} className='bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded'>
                 Read Data
-            </button> */}
+            </button>
 
 
 
