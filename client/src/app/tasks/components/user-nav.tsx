87e09d5f394f284da99048ae4eb860e582e1e3a8
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-10-14 12:37:01 +0530

    made ui for showing tasks

---
+++ client/src/app/tasks/components/user-nav.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { addressResolverAbi } from "viem/_types/constants/abis";
+import { useAccount } from "wagmi";
+
+export function UserNav() {
+  const { address } = useAccount();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          {address ? (
+            <Avatar className="h-9 w-9">
+              <AvatarImage
+                src={`https://api.dicebear.com/5.x/bottts/svg?seed=${address}`}
+                alt="@deano"
+              />
+              <AvatarFallback>DE</AvatarFallback>
+            </Avatar>
+          ) : (
+            <Avatar className="h-9 w-9">
+              <AvatarImage src="/avatars/03.png" alt="@shadcn" />
+              <AvatarFallback>SC</AvatarFallback>
+            </Avatar>
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              m@example.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
+          </DropdownMenuItem>
+          {/* <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>New Team</DropdownMenuItem>*/}
+        </DropdownMenuGroup>
+        {/* <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>  */}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
