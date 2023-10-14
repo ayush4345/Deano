4623b3dde1e3d510849eb862846ab6b3dc8068ae
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-10-15 02:38:56 +0530

    improved ui of vendors

--- client/src/app/tasks/page.tsx
+++ client/src/app/tasks/page.tsx
@@ -21,23 +21,7 @@ export default async function TaskPage() {
 
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/tasks-light.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/tasks-dark.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="h-full flex-1 flex-col space-y-8 p-8 flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
             <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
