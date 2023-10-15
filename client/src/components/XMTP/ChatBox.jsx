3a9c4a102f9d2598b52aed05ff2381385dd591ca
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-10-15 18:21:11 +0530

    changed chatbox ui

--- client/src/components/XMTP/ChatBox.jsx
+++ client/src/components/XMTP/ChatBox.jsx
@@ -31,49 +31,38 @@ const ChatBox = ({ client, messageHistory, conversation, setShowContactList, sel
       (v, i, a) => a.findIndex((t) => t.id === v.id) === i,
     );
 
-  const getUserName = (message) => {
-    if(message.senderAddress === address) {
-      return "You"
-    } else if(selectedContact && selectedContact.profileName !== "No web3 profile") {
-      return "Vendor" 
-    } else if(selectedContact && selectedContact.address) {
-      return "Vendor"
-    } else {
-      return "Vendor"
-    }    
-  }
+    const getUserName = (message) => {
+      if (message.senderAddress === address) {
+        return "You"
+      } else if (selectedContact && selectedContact.profileName !== "No web3 profile") {
+        return "Vendor"
+      } else if (selectedContact && selectedContact.address) {
+        return "Vendor"
+      } else {
+        return "Vendor"
+      }
+    }
 
     return (
-<div className=" ">
-      <ul className=" m-2 p-3  overflow-y-scroll no-scrollbar max-h-[500px] max-w-[500px] ">
-        {messages.map((message, index) => (
-          
-          <li
-            key={message.id}
-            className={`m-2 p-2 border-2 rounded-md ${getUserName(message) === "You" ? " text-right bg-green-200" : "text-left bg-white"}`}
+      <div className=" ">
+        <ul className=" m-2 p-3  overflow-y-scroll no-scrollbar max-h-[500px] max-w-[500px] ">
+          {messages.map((message, index) => (
+
+            <li
+              key={message.id}
+              className={`m-2 p-2 rounded-t-lg ${getUserName(message) === "You" ? "text-right rounded-l-lg bg-[#3D5A66] text-white" : "text-left rounded-r-lg bg-white"}`}
             >
-
               <div className="flex flex-col ">
-
-            <strong className="text-green-900">
-              {/* {getUserName(message)} */}
-            </strong>
-
-            <span className="flex-wrap">{message.content}</span>
-
-            <span className="text-[12px]"> ({message.sent.toLocaleTimeString()})</span>
-
-
+                <strong className="text-green-900">
+                  {/* {getUserName(message)} */}
+                </strong>
+                <span className="flex-wrap">{message.content}</span>
+                <span className="text-[12px] text-[#81A8B1]"> {message.sent.toLocaleTimeString()}</span>
               </div>
-
-
-
-            
-          </li>
-        ))}
-      </ul>
-
-</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   };
 
@@ -88,7 +77,7 @@ const ChatBox = ({ client, messageHistory, conversation, setShowContactList, sel
   return (
     <div>
 
-      <h1 className="font-bold text-white mx-auto flex justify-center items-center text-[30px] m-1 p-1">Chat with Vendor</h1>
+      <h1 className="font-semibold text-[#202009] bg-[#E9ECED] mx-auto w-full flex justify-center items-center text-[30px] rounded-t-lg shadow-md">chat with vendor</h1>
 
       {/* <button onClick={() => setShowContactList(true)}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -96,23 +85,23 @@ const ChatBox = ({ client, messageHistory, conversation, setShowContactList, sel
         </svg>
       </button> */}
 
-{/* div for the Message List */}
+      {/* div for the Message List */}
       <div className="">
-        <MessageList messages={messageHistory}/>
+        <MessageList messages={messageHistory} />
       </div>
 
-{/* div for the Input and Send Button */}
+      {/* div for the Input and Send Button */}
 
-      <div className="m-2 p-2 flex items-center justify-center">
+      <div className=" m-auto mx-5 mb-5 bg-slate-50 rounded-md border-2 border-transparent focus-within:border-2 focus-within:border-[#6796A2] flex items-center justify-center">
         <input
           type="text"
-          className="p-3 m-4 w-full border-2 border-white rounded-md"
+          className="p-3 w-full bg-slate-50 rounded-md outline-none "
           onKeyPress={handleInputChange}
           onChange={handleInputChange}
           value={inputValue}
           placeholder="Type a message..."
         />
-        <button onClick={handleSend} className="p-3 m-1 bg-rose-600 border-2 border-white rounded-md hover:bg-green-500 transition-all duration-300 ease-in-out text-white ">
+        <button onClick={handleSend} className="p-3 m-1 bg-[#62929E] border-2 border-white rounded-md hover:bg-[#466972] transition-all duration-300 ease-in-out text-white ">
           <PaperPlaneIcon />
         </button>
       </div>
