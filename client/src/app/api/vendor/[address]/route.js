d730b12015766e634e6528aade33cdc73ec48980
Shubham-Rasal <bluequbits@gmail.com>
2023-10-16 19:11:08 +0530

    package updates

--- client/src/app/api/vendor/[address]/route.js
+++ client/src/app/api/vendor/[address]/route.js
@@ -1,27 +1,12 @@
-import { getAllJobs, getVendorJobs } from '@/tableland/db'
 import { NextResponse } from 'next/server'
 
-const job = {
-    bounty: 112,
-    cid: 'bafybeihi4eb6t32szzhxcp7gxrspwcpviktqy7o7s2qagfnqigd7moicri',
-    job_id: 3,
-    status: 1,
-    type: 2,
-    vendor_address: '0x3151D7f15e6cdfEeeA36D0dE4F6Da118f7A757e7'
-}
-
 export async function GET(request, { params }) {
-    const vendor_address = params.address
-
-    const jobs = await getVendorJobs(vendor_address);
-    console.log(jobs);
-    //todo 
+      //todo 
     //filter jobs by vendor address
-
+    const vendor_address = params.address
     const vendor_details = {
         name: 'test',
         address: vendor_address,
-        jobs
     }
 
     return NextResponse.json(vendor_details)
