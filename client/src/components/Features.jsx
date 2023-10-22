84f08ae706a22488f0cd4018703da9d58fd33e9e
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-10-22 11:36:20 +0530

    home content : Add content

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -24,165 +24,141 @@ export default function Features() {
             a decentralised way.
           </p>
         </div>
-        <div className="mt-16 grid divide-x divide-y divide-gray-100 dark:divide-gray-700 overflow-hidden rounded-3xl border border-gray-100 text-gray-600 dark:border-gray-700 sm:grid-cols-2 lg:grid-cols-4 lg:divide-y-0 xl:grid-cols-4">
-          <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-            <div className="relative space-y-8 py-12 p-8">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/4341/4341139.png"
-                className="w-12"
-                width="512"
-                height="512"
-                alt="burger illustration"
-              />
+        <div className="mt-16 mb-16 grid divide-x divide-y divide-gray-100 dark:divide-gray-700 overflow-hidden rounded-3xl border border-gray-100 text-gray-600 dark:border-gray-700 sm:grid-cols-2 lg:grid-cols-4 lg:divide-y-0 xl:grid-cols-4">
+          {/* Push Protocol */}
+                    <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+                      <div className="relative space-y-8 py-12 p-8">
+                        <img
+                          src="https://img.icons8.com/nolan/64/appointment-reminders.png"
+                          className="w-12"
+                          width="512"
+                          height="512"
+                          alt="notification icon"
+                        />
 
-              <div className="space-y-2">
-                <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
-                  First feature
-                </h5>
-                <p className="text-gray-600 dark:text-gray-300">
-                  Neque Dolor, fugiat non cum doloribus aperiam voluptates
-                  nostrum.
-                </p>
-              </div>
-              <a
-                href="#"
-                className="flex items-center justify-between group-hover:text-orange-700"
-              >
-                <span className="text-sm">Read more</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-            <div className="relative space-y-8 py-12 p-8">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/4341/4341134.png"
-                className="w-12"
-                width="512"
-                height="512"
-                alt="burger illustration"
-              />
+                        <div className="space-y-2">
+                          <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
+                            Notification using Push protocol
+                          </h5>
+                          <p className="text-gray-600 dark:text-gray-300">
+                            A vendor on creating a new job will directly notify the annotator in their inboxes.
+                          </p>
+                        </div>
+                        
+                      </div>
+                    </div>
+          {/* XMTP Chat */}
+                    <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+                      <div className="relative space-y-8 py-12 p-8">
+                        <img
+                          src="https://img.icons8.com/doodle/48/chat.png"
+                          className="w-12"
+                          width="512"
+                          height="512"
+                          alt="chat icon"
+                        />
 
-              <div className="space-y-2">
-                <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
-                  Second feature
-                </h5>
-                <p className="text-gray-600 dark:text-gray-300">
-                  Neque Dolor, fugiat non cum doloribus aperiam voluptates
-                  nostrum.
-                </p>
-              </div>
-              <a
-                href="#"
-                className="flex items-center justify-between group-hover:text-orange-700"
-              >
-                <span className="text-sm">Read more</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-            <div className="relative space-y-8 py-12 p-8">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/4341/4341160.png"
-                className="w-12"
-                width="512"
-                height="512"
-                alt="burger illustration"
-              />
+                        <div className="space-y-2">
+                          <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
+                            XMTP Chat
+                          </h5>
+                          <p className="text-gray-600 dark:text-gray-300">
+                            Stay connected with the Vendor for latest Job updates and related queries.
+                          </p>
+                        </div>
+                        
+                      </div>
+                    </div>
+          {/* Privy */}
+                    <div className="group relative bg-white dark:bg-gray-900 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+                      <div className="relative space-y-8 py-12 p-8 transition duration-300 group-hover:bg-white dark:group-hover:bg-gray-800">
+                        <img
+                          src="https://img.icons8.com/ios/50/remove-fingerprint.png"
+                          className="w-12"
+                          width="512"
+                          height="512"
+                          alt="authentication icon"
+                        />
 
-              <div className="space-y-2">
-                <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
-                  Third feature
-                </h5>
-                <p className="text-gray-600 dark:text-gray-300">
-                  Neque Dolor, fugiat non cum doloribus aperiam voluptates
-                  nostrum.
-                </p>
-              </div>
-              <a
-                href="#"
-                className="flex items-center justify-between group-hover:text-orange-700"
-              >
-                <span className="text-sm">Read more</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="group relative bg-white dark:bg-gray-900 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-            <div className="relative space-y-8 py-12 p-8 transition duration-300 group-hover:bg-white dark:group-hover:bg-gray-800">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/4341/4341025.png"
-                className="w-12"
-                width="512"
-                height="512"
-                alt="burger illustration"
-              />
+                        <div className="space-y-2">
+                          <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
+                          Seemless Authentication using Privy
+                          </h5>
+                          <p className="text-gray-600 dark:text-gray-300">
+                          Privy brings web2-caliber UX — like sign in with email and social — to web3 products. Don't just increase conversion, increase the amount of potential users for your app.
+                          </p>
+                        </div>
+                        
+                      </div>
+                    </div>
+          {/* Sismo */}
+                    <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+                      <div className="relative space-y-8 py-12 p-8">
+                        <img
+                          src="https://img.icons8.com/nolan/64/code--v2.png"
+                          className="w-12"
+                          width="512"
+                          height="512"
+                          alt="sismo icon"
+                        />
 
-              <div className="space-y-2">
-                <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
-                  More features
-                </h5>
-                <p className="text-gray-600 dark:text-gray-300">
-                  Neque Dolor, fugiat non cum doloribus aperiam voluptates
-                  nostrum.
-                </p>
-              </div>
-              <a
-                href="#"
-                className="flex items-center justify-between group-hover:text-orange-700"
-              >
-                <span className="text-sm">Read more</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
+                        <div className="space-y-2">
+                          <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
+                            Sismo
+                          </h5>
+                          <p className="text-gray-600 dark:text-gray-300">
+                          Sismo details
+                          </p>
+                        </div>
+                        
+                      </div>
+                    </div>
+          {/* Tableland */}
+                    <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+                      <div className="relative space-y-8 py-12 p-8">
+                        <img
+                          src="https://img.icons8.com/pulsar-color/48/table-1.png"
+                          className="w-12"
+                          width="512"
+                          height="512"
+                          alt="tableland icon"
+                        />
+
+                        <div className="space-y-2">
+                          <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
+                            Tableland
+                          </h5>
+                          <p className="text-gray-600 dark:text-gray-300">
+                          Tableland details
+                          </p>
+                        </div>
+                        
+                      </div>
+                    </div>
+          {/* Graph */}
+                    <div className="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+                      <div className="relative space-y-8 py-12 p-8">
+                        <img
+                          src="https://img.icons8.com/dusk/64/graph.png"
+                          className="w-12"
+                          width="512"
+                          height="512"
+                          alt="graph icon"
+                        />
+
+                        <div className="space-y-2">
+                          <h5 className="text-xl font-semibold text-gray-700 dark:text-white transition group-hover:text-orange-700">
+                            Graph
+                          </h5>
+                          <p className="text-gray-600 dark:text-gray-300">
+                          Graph details
+                          </p>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            );
+          }
