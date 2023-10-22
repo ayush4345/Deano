9ca721d954b7436ed5710de2375eebcff92cafee
Shubham-Rasal <bluequbits@gmail.com>
2023-10-22 18:33:27 +0530

    added sismo and removed usewallet

--- client/src/app/api/sismo/route.js
+++ client/src/app/api/sismo/route.js
@@ -1,26 +1,28 @@
 import { NextResponse } from 'next/server'
 // import {readData} from '../../../tableland/connect.js'
 import data from './data.json'
+import { getAnnotators } from '../../../tableland/db';
 export async function GET(request) {
 
-    // let annotators = await readData();   
+    let annotators = await getAnnotators();   
 
-    // annotators = annotators.map((annotator) => {
-    //     return {
-    //         name: annotator.name,
-    //         id: annotator.id
-    //     }
-    // })
+    //convert the reputation to string
+    annotators = annotators.map((annotator) => {
+        return {
+            name: annotator.address,
+            id: annotator.reputation ? annotator.reputation.toString() : 0
+        }
+    })
 
-    // const result = {}
+    const result = {}
 
-    // annotators.forEach((annotator) => {
-    //     result[annotator.name] = annotator.id.toString()
-    // })
+    annotators.forEach((annotator) => {
+        result[annotator.name] = annotator.id.toString()
+    })
 
-    // console.log(result)
+    console.log(result)
 
-    return NextResponse.json(data)
+    return NextResponse.json(result)
 
     // return NextResponse.json(result)
 }
